3b62c8c50de6267764f040b87c9eb84639bc94a3
rdavidson634 <rdavidson634@gmail.com>
2020-11-18 16:41:10 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../App';
+
+export const Header = () => {
+    return (
+        <h2>
+            Expense Tracker
+        </h2>
+    )
+}
+
+export default App;
