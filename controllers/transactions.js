bc5c950c0afed58c406ed14b2f15c561ca469b66
rdavidson634 <rdavidson634@gmail.com>
2020-11-19 14:27:22 -0700

    refactor

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -30,8 +30,19 @@ exports.addTransaction = async (req, res, next) => {
             data: transaction
         });
     } catch (err) {
-
-
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(val => val.message);
+            
+            return res.status(400).json({
+                success: false,
+                error: messages
+            })
+        } else {
+            return res.status(500).json({
+                success: false,
+                error: 'server error'
+            });
+        }
     }
 }
     
